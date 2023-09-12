4dd3b82d91fa0679fc0253c96dfb1cff25bb8332
ViniciusApBrito <vinicius.brito355@gmail.com>
2023-09-11 22:08:19 -0300

    Configure Git to not auto-convert line endings for pom.xml

--- src/main/java/com/example/studentbackend/resources/exceptions/ValidationErrors.java
+++ src/main/java/com/example/studentbackend/resources/exceptions/ValidationErrors.java
@@ -1,5 +1,17 @@
 package com.example.studentbackend.resources.exceptions;
 
-public class ValidationErrors {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationErrors extends StandardError {
+ private List<String> errors = new ArrayList<>();
+
+ public void addError(String error){
+    this.errors.add(error);
+ }
+
+public List<String> getErrors() {
+    return errors;
+}
+ 
 }
